2981f53734d8e955e1bfef1a5fcdfb5ae300db9c
newcoder2019 <ossamawi@gmail.com>
2019-01-24 20:35:27 -0800

    Heroku deployment

---
+++ client/build/precache-manifest.8795d513ecea18772ee1c0bd1365f7ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1257847d7f09e952e9a6",
+    "url": "/static/js/main.1257847d.chunk.js"
+  },
+  {
+    "revision": "9057d39878ab7fa3c04b",
+    "url": "/static/js/1.9057d398.chunk.js"
+  },
+  {
+    "revision": "1257847d7f09e952e9a6",
+    "url": "/static/css/main.aa20f506.chunk.css"
+  },
+  {
+    "revision": "3eb08cb50bd2a4baa300402963847fb1",
+    "url": "/index.html"
+  }
+];
